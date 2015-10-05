8b122d7e90e6cd5f804f307557a6d613fd77e1ce
obble <mkentnotpaul@gmail.com>
2015-10-05 18:53:24 +0100

    1
    + initial commit

---
+++ list.lua
@@ -0,0 +1,32 @@
+
+
+    -- here you can add, take-away and style spells you want to track.
+    -- this can easily be added to
+    -- it will NOT currently handle cooldowns.
+
+    modspells = {
+       {    -- PRIEST               -- KEY
+            priority = 1,           -- controls positioning of the string. lower number means it will stack above those w/ a higher one.
+            show = true,            -- toggle whether this aura will be tracked.
+            Name = 'Renew',         -- aura name.
+            symbol = '+',           -- symbol or text you want to represent the aura with.
+            size = 24,              -- size of text.
+            colour = { 0, 1, 0 },   -- RGB colour of text, can be numeric (0-1) or out of 255. in this case it's a nice yellow.
+        },
+        {
+            priority = 2,
+            show = true,
+            Name = 'Power Word: Shield',
+            symbol = '( )',
+            size = 20,
+            colour = { 1, .8, .3 },
+        },
+        {
+            priority = 2,
+            show = true,
+            Name = 'Fear Ward',
+            symbol = '‡',
+            size = 20,
+            colour = { 1, 1, 1 },
+        },
+    }
---
+++ spell.lua
@@ -0,0 +1,78 @@
+
+
+    local function tlength(t)
+        local count = 0
+        for _ in pairs(t) do count = count + 1 end
+        return count
+    end
+
+    for i = tlength(modspells), 1, -1 do    -- SCORE INFO
+        local spell = modspells[i]
+        if not spell.priority then spell.priority = 1 end
+        spell.name = spell.Name
+        if not spell.name then
+            print('modups: |cff01DFD7Spell', spell.id, 'doesn\'t exist! Check your info is correct in listofspells.lua.')
+            tremove(modspells, i)
+        end
+    end
+
+    local function sortSpells(a, b)         -- SORT
+            -- show lower priority first, or alphabetically within same priority
+        return a.priority < b.priority or a.priority == b.priority and a.name < b.name
+    end
+
+    table.sort(modspells, sortSpells)
+
+    local f = CreateFrame'Frame'            -- IMPLEMENT
+    f:RegisterEvent'PLAYER_ENTERING_WORLD'
+    f:RegisterEvent'PLAYER_AURAS_CHANGED'
+
+    local fontstrings = setmetatable({}, { __index = function(t, i)
+        local line = UIParent:CreateFontString(nil, 'OVERLAY', 'GameFontNormal')
+        if i > 1 then
+            line:SetPoint('BOTTOMRIGHT', t[i - 1], 'TOPRIGHT', 0, 10)
+        else
+            line:SetPoint('RIGHT', UIParent, 'CENTER', 96, 75)
+        end
+        line:SetJustifyH'RIGHT'
+        line:SetShadowOffset(0, 0)
+        line:SetSpacing(2)
+        t[i] = line
+        return line
+    end})
+
+    f:SetScript('OnEvent', function()
+        local i = 0
+        local shown = 1
+    	while true do
+            GameTooltip:SetOwner(UIParent, 'ANCHOR_NONE')
+            GameTooltip:ClearLines()
+            GameTooltip:SetPlayerBuff(GetPlayerBuff(i, 'HELPFUL'))
+            local buff  = GameTooltipTextLeft1:GetText()
+            local stack = GetPlayerBuffApplications(i)
+
+            for i = shown, tlength(fontstrings) do
+                fontstrings[i]:Hide()
+            end
+
+            for j = 1, tlength(modspells) do
+                local spell = modspells[j]
+                if buff and spell.name == buff then
+                    if spell.show == true then
+                        local fontstring = fontstrings[shown] -- keep them all in another indexed table
+                        fontstring:SetText(string.rep(spell.symbol, stack > 0 and stack or 1))
+                        fontstring:SetFont(STANDARD_TEXT_FONT, spell.size, 'OUTLINE')
+                        fontstring:SetTextColor(spell.colour[1], spell.colour[2], spell.colour[3])
+                        fontstring:Show()
+                        shown = shown + 1
+                    end
+                end
+            end
+
+            GameTooltip:Hide()
+            if not buff then break end
+            i = i + 1
+        end
+    end)
+
+    --
